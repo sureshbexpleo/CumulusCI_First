68e0a615d1b577264cdb2ec2f5c7a1f83fb20856
suresh <sureshbabu.rajamanickam@expleogroup.com>
2023-03-14 20:43:13 +0530

    jira 19

--- jira.sh
+++ jira.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-#printenv
-#curl -H "Content-Type: text/xml" -X POST -H "Authorization: Bearer ${{secrets.JIRA_TOKEN}}"  --data @/home/runner/work/CumulusCI_First/CumulusCI_First/output/output.xmlhttps://devstack.vwgroup.com/jira/rest/raven/2.0/import/execution/robot?testExecKey=${JIRA_EXECUTION_ID}
-STATUS_CODE=$(curl -LI -H "Content-Type: text/xml" -X POST -H "Authorization: Bearer ${JIRA_TOKEN}"  --data @/home/runner/work/CumulusCI_First/CumulusCI_First/output/output.xml https://devstack.vwgroup.com/jira/rest/raven/2.0/import/execution/robot?testExecKey=$JIRA_EXECUTION_ID -o /dev/null -w '%{http_code}\n' -s)
+printenv
+STATUS_CODE=$(curl -H "Content-Type: multipart/form-data" -u $JIRA_USERNAME:$JIRA_PASSWORD -F "file=@/home/runner/work/CumulusCI_First/CumulusCI_First/output/output.xml" "$JIRA_SERVER_URL/rest/raven/2.0/import/execution/robot?projectKey=${PROJECT_KEY}&testExecKey=${JIRA_EXECUTION_ID}" -o /dev/null -w '%{http_code}\n' -s)
 if [[ "$STATUS_CODE" -ne 200 ]] ; then
   echo "Site status changed to $STATUS_CODE"
 else
